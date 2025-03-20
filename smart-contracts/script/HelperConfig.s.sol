310c1e3be37b98dfcf123e82e35b12f93d0a2ace
fraolb <Fraolbereket@gmail.com>
2025-03-20 19:27:14 +0300

    update helper config to add celo config

--- smart-contracts/script/HelperConfig.s.sol
+++ smart-contracts/script/HelperConfig.s.sol
@@ -25,24 +25,68 @@ contract HelperConfig is Script {
     //////////////////////////////////////////////////////////////*/
 
     uint256 constant LOCAL_CHAIN_ID = 31337;
+    uint256 constant CELO_MAINNET_CHAIN_ID = 42220;
+    uint256 constant CELO_ALFAJORES_CHAIN_ID = 44787;
+
     // address constant FOUNDRY_DEFAULT_WALLET = 0x1804c8AB1F12E6bbf3894d4083f33e07309d1f38;
     address constant ANVIL_DEFAULT_ACCOUNT =
         0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
+    address constant BURNER_WALLET = 0x643315C9Be056cDEA171F4e7b2222a4ddaB9F88D;
 
     NetworkConfig public localNetworkConfig;
+    mapping(uint256 chainId => NetworkConfig) public networkConfigs;
 
     /*//////////////////////////////////////////////////////////////
                                FUNCTIONS
     //////////////////////////////////////////////////////////////*/
-    constructor() {}
+    constructor() {
+        networkConfigs[CELO_MAINNET_CHAIN_ID] = getCeloMainnetConfig();
+        networkConfigs[
+            CELO_ALFAJORES_CHAIN_ID
+        ] = getCeloAlfajoresTestnetConfig();
+    }
 
     function getConfig() public returns (NetworkConfig memory) {
-        return getOrCreateAnvilEthConfig();
+        return getConfigByChainId(block.chainid);
+    }
+
+    function getConfigByChainId(
+        uint256 chainId
+    ) public returns (NetworkConfig memory) {
+        if (chainId == LOCAL_CHAIN_ID) {
+            return getOrCreateAnvilEthConfig();
+        } else if (networkConfigs[chainId].account != address(0)) {
+            return networkConfigs[chainId];
+        } else {
+            revert HelperConfig__InvalidChainId();
+        }
     }
 
     /*//////////////////////////////////////////////////////////////
                                 CONFIGS
     //////////////////////////////////////////////////////////////*/
+
+    function getCeloMainnetConfig() public pure returns (NetworkConfig memory) {
+        return
+            NetworkConfig({
+                entryPoint: 0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789,
+                usdc: 0x48065fbBE25f71C9282ddf5e1cD6D6A887483D5e,
+                account: BURNER_WALLET
+            });
+    }
+
+    function getCeloAlfajoresTestnetConfig()
+        public
+        pure
+        returns (NetworkConfig memory)
+    {
+        return
+            NetworkConfig({
+                entryPoint: 0x0000000071727De22E5E9d8BAf0edAc6f37da032,
+                usdc: 0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1,
+                account: BURNER_WALLET
+            });
+    }
 
     function getOrCreateAnvilEthConfig() public returns (NetworkConfig memory) {
         if (localNetworkConfig.account != address(0)) {
